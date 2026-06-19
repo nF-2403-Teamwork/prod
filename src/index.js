2b76f79dfc979c95c503670cace1fd988a483040
bekzodmirzaaliyevdev <bekzod.mirzaaliyev@icloud.com>
2026-06-19 17:20:52 +0500

    ASD

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+app.use(cors());
+app.use(express.json());
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("message", (data) => {
+    io.emit("message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Server ishlayapti 🚀");
+});
+
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
